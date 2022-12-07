de465d892f1f0060e1d017d10bad0d6196ec2c75
Artem <kypidon71@gmail..com>
2022-12-07 22:28:49 +0300

    Add proverki in 2 zada4a

--- zada4ki/002.py
+++ zada4ki/002.py
@@ -12,11 +12,40 @@ def printSetka (set):
 
 print(printSetka(setka))
 
-while True:
-    per1 = int(input('Введи позицию для крестика X:= ') )
-    setka[per1-1] = 'X'
-    print(printSetka(setka))
-    per2 = int(input('Введи позицию для крестика 0:= ') )
-    setka[per2-1] = '0'
-    print(printSetka(setka))
+def take_input (player_token):    #оргумент функции
+    valid = False
+    while not valid:   #меняет результат тоесть если будет тру то он поменяет на фолс и обратно
+        player_answer = input(' куда поставим ' + player_token + "?")
+
+        try:
+            player_answer = int(player_answer)
+        except:
+            print('не корректный ввод')
+            continue
+        if player_answer >= 1 and player_answer <= 9 :
+            if(str(setka[player_answer -1]) not in 'XO'):
+                setka[player_answer -1] = player_token
+                valid = True
+            else:
+                print('клетка уже занята ')
+        else:
+            print('введите число от 1 до 9')
+
+
+
+
+
+
+
+
+
+# while True:
+#     per1 = int(input('Введи позицию для крестика X:= ') )
+#     setka[per1-1] = 'X'
+#     print(printSetka(setka))
+#     per2 = int(input('Введи позицию для крестика 0:= ') )
+#     setka[per2-1] = '0'
+#     print(printSetka(setka))
+#     if per1 == setka[per1-1] and per2 == setka[per2-1] :
+#         print('Эта позиция уже занята!')
     
